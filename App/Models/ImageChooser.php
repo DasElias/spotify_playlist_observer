d68ec65e90ab59317ef5b261986b968ef3db3af0
Elias Horner <elias.horner@student.htldornbirn.at>
2021-04-23 09:16:11 +0200

    feature: use new image chooser strategy

--- App/Models/ImageChooser.php
+++ App/Models/ImageChooser.php
@@ -8,9 +8,20 @@ class ImageChooser {
 
     foreach($images as $i) {
       $delta = abs($i["width"] - $targetSize);
-      if($delta < $minDelta) {
+      if($delta < $minDelta && $i["width"] > $targetSize) {
         $minDelta = $delta;
         $minDeltaUrl = $i["url"];
+      }
+    }
+
+    // only return smaller images when there are no bigger ones
+    if($minDeltaUrl == null) {
+      foreach($images as $i) {
+        $delta = abs($i["width"] - $targetSize);
+        if($delta < $minDelta) {
+          $minDelta = $delta;
+          $minDeltaUrl = $i["url"];
+        }
       }
     }
 
