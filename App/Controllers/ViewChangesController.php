0d3560a736690c787480ff0dd95e63cdbb88b1f1
Elias Horner <elias.horner@student.htldornbirn.at>
2021-04-24 11:52:54 +0200

    remove commented code

--- App/Controllers/ViewChangesController.php
+++ App/Controllers/ViewChangesController.php
@@ -9,27 +9,12 @@ class ViewChangesController extends AbstractController {
     parent::__construct();
 
   }
-/*
-  private function setSessionFromGet($key) {
-    if(isset($_GET[$key])) {
-      $_SESSION[$key] = $_GET[$key];
-      $this->redirect("viewChanges.php?id=" . $_GET["id"]);
-      die;
-    } 
-  }*/
-
 
   public function show() {
     if(! isset($_GET["id"])) {
       $this->redirect("listPlaylists.php");
       return;
     }
-    
-/*    $this->setSessionFromGet("acceptSongUri");
-    $this->setSessionFromGet("declineSongUri");
-    $this->setSessionFromGet("acceptAll");
-    $this->setSessionFromGet("declineAll");*/
-
 
     try {
       $dbService = new DatabaseService(); 
@@ -40,30 +25,6 @@ class ViewChangesController extends AbstractController {
       }
 
       $spotifyService = new ApiSpotifyService();
-   /*   if(isset($_SESSION["acceptSongUri"])) {
-        $acceptSongUri = $_SESSION["acceptSongUri"];
-        $spotifyService->addSongsToPlaylist($playlist->getDestId(), [
-          $acceptSongUri
-        ]);
-        $playlist->removeSongFromChanges($acceptSongUri);
-        unset($_SESSION["acceptSongUri"]);
-      }
-      if(isset($_SESSION["declineSongUri"])) {
-        $declineSongUri = $_SESSION["declineSongUri"];
-        $playlist->removeSongFromChanges($declineSongUri);
-        unset($_SESSION["declineSongUri"]);
-      }
-      if(isset($_SESSION["acceptAll"])) {
-        $uris = $playlist->removeAllChangesAndGetUris();
-        $spotifyService->addSongsToPlaylist($playlist->getDestId(), $uris);
-        unset($_SESSION["acceptAll"]);
-      }
-      if(isset($_SESSION["declineAll"])) {
-        $uris = $playlist->removeAllChangesAndGetUris();
-        $spotifyService->addSongsToPlaylist($playlist->getDestId(), $uris);
-        unset($_SESSION["declineAll"]);
-      }*/
-
       $sourcePlaylist = $spotifyService->getPlaylist($playlist->getSourceId());
       $destPlaylist = $spotifyService->getPlaylist($playlist->getDestId());
       $playlist->update($sourcePlaylist, $destPlaylist);
