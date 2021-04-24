268bb0bbb6b356277e217f80c12a4dc22066124e
Elias Horner <elias.horner@student.htldornbirn.at>
2021-04-24 11:38:35 +0200

    bugfix: fix changes dropdown on mobile devices

--- scripts/changes-dropdown.js
+++ scripts/changes-dropdown.js
@@ -1,7 +1,7 @@
 $(function() {
   // check if dropdown exists and is touch
   var isTouch = "ontouchstart" in window || navigator.msMaxTouchPoints;
-  if($(".dropdown").length && isTouch) {
+  if($(".dropdown").length) {
     $(".dropdown").each(function() {
       var dd = $(this);
       var btn = dd.find(".dropdown-toggle");
@@ -10,7 +10,7 @@ $(function() {
       });
     });
 
-    $(window).click(function(event) {
+    $(document).on('click touch', function(event) {
       var target = $(event.target);
       if(! target.parents(".dropdown").length) {
         $(".dropdown").removeClass("dropdown-visible");
