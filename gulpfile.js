58590271a1b60d5cd771c17a76b2a0e7f757279b
Elias Horner <elias.horner@student.tuwien.ac.at>
2022-10-31 23:10:34 +0100

    feat: add lit

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var postcss = require('gulp-postcss');
 const rename = require('gulp-rename');
+const rollup = require('rollup');
+const resolve = require('rollup-plugin-node-resolve');
+const typescript = require('rollup-plugin-typescript');
 const uglify = require('gulp-terser');
 const cleanCSS = require('gulp-clean-css');//To Minify CSS files
 const del = require('del');
@@ -34,6 +37,30 @@ function scripts() {
 
 }
 
+async function buildLitScript() {
+    const bundle = await rollup
+        .rollup({
+            input: ['lit/lit-main.js'],
+            output: {
+                file: 'public/dist/lit_index.js',
+                format: 'es',
+                sourcemap: true
+            },
+            plugins: [
+                resolve(),
+                typescript()
+            ]
+        });
+
+    return bundle.write({
+        file: './public/dist/lit_index.js',
+        format: 'umd',
+        name: 'library',
+        sourcemap: true
+    });
+    
+}
+
 function clean() {
     return del("./public/dist");
 }
@@ -45,14 +72,14 @@ function buildFinish(done) {
 
 exports.build = gulp.series(
     clean,
-    gulp.parallel(styles, scripts),
+    gulp.parallel(styles, scripts, buildLitScript),
     buildFinish
 )
 
-exports.default = function() {
-    gulp.watch('styles/**/*.scss', gulp.series(
+exports.watch = function() {
+    gulp.watch(['lit/**/*.*', 'styles/**/*.scss'], gulp.series(
         clean,
-        gulp.parallel(styles, scripts),
+        gulp.parallel(styles, scripts, buildLitScript),
         buildFinish
     ));
 }
